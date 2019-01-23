f75afe59f76878de0d69288708965eb1e16051a1
waynewccheng <45103074+waynewccheng@users.noreply.github.com>
2019-01-23 06:33:55 -0500

    Home tested patterns

--- TG18-QC/hdrpatt.m
+++ TG18-QC/hdrpatt.m
@@ -5,17 +5,18 @@ im = zeros(1000,1000,3);
 k = 0;
 for i = 0:9
     for j = 0:9
-        lum = k*0.01;
+        jnd = 1 - k*(1/255);
+        lum = jnd .^ 2.2;
         
         row = i * 100 + 1;
         col = j * 100 + 1;
-        im(row:row+99,col:col+99,1) = lum;
+        im(row:row+99,col:col+99,1) = 1;
         im(row:row+99,col:col+99,2) = lum;
-        im(row:row+99,col:col+99,3) = lum;
+        im(row:row+99,col:col+99,3) = 1;
         k = k + 1;
     end
 end
 
 imagesc(im)
-hdrwrite(im,'myhdr.hdr');
+hdrwrite(im,'myhdr_per_255rc.hdr');
 
