87cf6dcb101ed161c585acbc04f03c899cec847b
Wayne Cheng <waynewccheng@gmail.com>
2019-01-24 15:01:13 -0500

    From lab

---
+++ TG18-QC/dicom2hdr_rotate.m
@@ -0,0 +1,18 @@
+% Q: how to create an HDR image (.exr) for HDRDisplay
+function dicom2hdr (fn)
+
+fn_in = [fn '.dcm'];
+fn_out = [fn '.hdr'];
+
+X = double(dicomread(fn_in))./4096;
+
+X = X';
+
+XX = X .^ 2.2;
+
+XX3 = repmat(XX,1,1,3);
+
+hdrwrite(XX3,fn_out)
+
+end
+
